f06705bae0d565fe04f1f032e83ba3947fbafa7f
Joey Smith <jsmith@webinertia.net>
2022-03-28 23:24:44 -0500

    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- public/index.php
+++ public/index.php
@@ -5,14 +5,11 @@ declare(strict_types=1);
 use Laminas\Mvc\Application;
 use Laminas\Stdlib\ArrayUtils;
 
-/**
- * This makes our life easier when dealing with paths. Everything is relative
- * to the application root now.
- */
+//use function php_sapi_name;
 
 chdir(dirname(__DIR__));
 // Decline static file requests back to the PHP built-in webserver
-if (php_sapi_name() === 'cli-server') {
+if (PHP_SAPI === 'cli-server') {
     $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
     if (is_string($path) && __FILE__ !== $path && is_file($path)) {
         return false;
