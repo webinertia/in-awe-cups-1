56fe38db7040946e85f5f7aa2080f8f68f7a05d8
Joey Smith <jsmith@webinertia.net>
2022-09-18 21:00:44 -0500

    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- module/App/test/Controller/IndexControllerTest.php
+++ module/App/test/Controller/IndexControllerTest.php
@@ -20,9 +20,9 @@ final class IndexControllerTest extends AbstractHttpControllerTestCase
         $configOverrides  = [
             'db' => [
                 'driver'   => 'pdo_mysql',
-                'dsn'      => 'mysql:dbname=testdb;host=localhost;charset=utf8',
-                'username' => 'root',
-                'password' => 'Password123',
+                'dsn'      => 'mysql:dbname=aurora;host=localhost;charset=utf8',
+                'username' => 'aurora',
+                'password' => 'password',
             ],
         ];
         $configOverrides += include __DIR__ . '/../../../../config/roles.php';
