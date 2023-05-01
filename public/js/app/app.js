230a4c924888e927e07a59253e4f5fb9b227d195
Joey Smith <jsmith@webinertia.net>
2023-05-01 12:22:16 -0500

    Signed-off-by: Joey Smith <jsmith@webinertia.net>

---
+++ public/js/app/app.js
@@ -0,0 +1,16 @@
+define([
+    "dojo/Stateful",
+    "dojo/_base/declare",
+    "dojo/topic",
+    "dojo/_base/Deferred",
+    "dojo/request/notify",
+    "dojo/dom",
+    "dojo/query",
+    "dojo/_base/array",
+    "dojo/dom-construct"
+],
+function(Stateful, declare, topic, Deferred, notify, dom, query, arrayUtil, domConstruct) {
+    // create App class, as subclass of Stateful
+        return declare("App", [Stateful], {
+    });
+});
