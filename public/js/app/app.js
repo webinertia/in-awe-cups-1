71f7e1df808f5179a4729690b903ce7b2b90da53
Joey Smith <jsmith@webinertia.net>
2023-05-02 02:09:48 -0500

    adding local copies of popper and moved some file locations added a admin.css for loading the required admin css isolated from the front end of the app added app.js moved the js resources out of the theme directory to support loading them via the dojo loader removed jsquery as a dependency as the move to dojo has begun since bootstrap no longer has a dependecy on jquery Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -3,14 +3,35 @@ define([
     "dojo/_base/declare",
     "dojo/topic",
     "dojo/_base/Deferred",
+    "dojo/request",
     "dojo/request/notify",
     "dojo/dom",
     "dojo/query",
     "dojo/_base/array",
-    "dojo/dom-construct"
+    "dojo/dom-construct",
+    "dojo/on",
+    "dojo/_base/lang",
+    "dojo/domReady!"
 ],
-function(Stateful, declare, topic, Deferred, notify, dom, query, arrayUtil, domConstruct) {
+function(Stateful, declare, topic, Deferred, request, notify, dom, query, arrayUtil, domConstruct, on, lang) {
     // create App class, as subclass of Stateful
-        return declare("App", [Stateful], {
+    return declare("App", [Stateful], {
+
+        init: () => {
+            topic.subscribe("system/message", function(e) {
+                let markup = domConstruct.toDom('<div class="alert alert-'+ e.type +' alert-dismissible" role="alert"><div>'+ e.message +'</div><button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>');
+                domConstruct.place(markup, 'system-message');
+            });
+            let menuHandler = {
+                id: "menuHandler",
+                onClick: evt => {
+                    evt.preventDefault();
+                    //
+                    topic.publish("system/message", {message: "Test message to test the alert system", type: "danger"});
+                }
+            };
+            let mainNav = dom.byId("main-nav");
+            on(mainNav, ".nav-link:click", lang.hitch(menuHandler, "onClick"));
+        }
     });
 });
